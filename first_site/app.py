c2d7fea6580b3b703952725d3bc80a20cce132fc
Matt <mattmills2110@gmail.com>
2019-08-06 00:51:57 +0100

    First hit

--- first_site/app.py
+++ first_site/app.py
@@ -11,9 +11,13 @@ def main():
 def register_blueprints():
     from first_site.views import home_views
     from first_site.views import package_views
+    from first_site.views import account_views
+    from first_site.views import cms_views
 
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(account_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
 
 
 if __name__ == '__main__':
--- first_site/views/package_views.py
+++ first_site/views/package_views.py
@@ -1,12 +1,15 @@
 import flask
 
-from Infrastructure.view_modifiers import response
-from services import package_service
-
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
-#@response(template_file="packages/details.html")
+# @response(template_file="packages/details.html")
 def package_details(package_name: str):
     return 'Package details for {}'.format(package_name)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    print(type(rank), rank)
+    return 'The details for the {}th most popular package'.format(rank)
