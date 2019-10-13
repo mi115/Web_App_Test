12f94023a9310e49551906c874bafcd66c0f111e
Matt <mattmills2110@gmail.com>
2019-10-13 10:39:51 +0100

    13.10.19

--- first_site/data/__all_models.py
+++ first_site/data/__all_models.py
@@ -1,4 +1,19 @@
-# noinspection PyUnresolvedReferences
-from first_site.data.project import Project
+# Add all your sqlalchemy models here.
+# This allows us to import just this file when
+# we need to preload the models and ensure they
+# are all loaded.
 
-#and all the new ones...
+# noinspection PyUnresolvedReferences
+import first_site.data.downloads
+# noinspection PyUnresolvedReferences
+import first_site.data.languages
+# noinspection PyUnresolvedReferences
+import first_site.data.licenses
+# noinspection PyUnresolvedReferences
+import first_site.data.maintainers
+# noinspection PyUnresolvedReferences
+import first_site.data.project
+# noinspection PyUnresolvedReferences
+import first_site.data.releases
+# noinspection PyUnresolvedReferences
+import first_site.data.users
