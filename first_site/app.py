45b0a3608f16863d9758e8cce0531d63ef38ddcd
Matt <mattmills2110@gmail.com>
2019-11-03 15:34:33 +0000

    Server Configuration

--- first_site/app.py
+++ first_site/app.py
@@ -1,20 +1,39 @@
 import os
+import sys
 import flask
 import first_site.data.db_session as db_session
+
+
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, folder)
 
 app = flask.Flask(__name__)
 
 
 def main():
+    configure()
+    app.run(debug=True, port=5006)
+
+
+def configure():
+    print("Configuring Flask app:")
+
     register_blueprints()
+    print("Registered blueprints")
+
     setup_db()
-    app.run(debug=True)
+    print("DB setup completed.")
+    print("", flush=True)
 
 
 def setup_db():
-    db_file = os.path.join(os.path.dirname(__file__), 'db', 'pypi.sqlite')
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pypi.sqlite')
 
     db_session.global_init(db_file)
+
 
 def register_blueprints():
     from first_site.views import home_views
@@ -32,3 +51,5 @@ def register_blueprints():
 
 if __name__ == '__main__':
     main()
+else:
+    configure()
